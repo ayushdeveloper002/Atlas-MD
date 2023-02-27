fbc82d5894081438512c57f79bd5d2a03880bd74
Ayush <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-02-27 09:24:43 +0530

    Update sticker.js

--- Commands/Utilities/sticker.js
+++ Commands/Utilities/sticker.js
@@ -22,21 +22,4 @@ module.exports = {
             const stickerBuffer = await stickerMess.toBuffer()
             Miku.sendMessage(m.from, {sticker:stickerBuffer}, { quoted: m })
         }
-        else if (/video/.test(mime)) {
-            let mediaMess = await quoted.download();
-            if ((quoted.msg || quoted).seconds > 15)  return Miku.sendMessage(m.from,{text:'Please send video less than 15 seconds.'},{quoted:m})
-            let stickerMess = new Sticker(mediaMess, {
-                pack: packname,
-                author: pushName,
-                type: StickerTypes.FULL,
-                categories: ['🤩', '🎉'],
-                id: '12345',
-                quality: 70,
-                background: 'transparent'
-            });
-            const stickerBuffer2 = await stickerMess.toBuffer()
-             Miku.sendMessage(m.from, {sticker:stickerBuffer2}, { quoted: m })
-    }else{
-        Miku.sendMessage(m.from,{text:`Please mention an *image/video* and type *${prefix}s* to create sticker.`},{quoted:m})
-    } 
 }}
