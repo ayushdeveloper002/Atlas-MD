a4cf3a7d4faa4e9aec23370f7a9bacb688a703b2
ayushdeveloper002 <119794276+ayushdeveloper002@users.noreply.github.com>
2023-02-27 09:09:02 +0530

    Update Core.js

--- Core.js
+++ Core.js
@@ -412,21 +412,9 @@ module.exports = async (Miku, m, commands, chatUpdate, store) => {
             }
         }
 
-        //----------------------------------------------------------------------------------------------------------------//
+        //-------------------------------------------------------------------------------------------------------------
 
 
-
-
-
-        const flags = args.filter((arg) => arg.startsWith("--"));
-        if (body.startsWith(prefix) && !icmd) {
-            let mikutext = `No such command programmed *${pushname}* senpai! Type *${prefix}help* or press the button below to get my full command list!\n`;
-
-            let Button = [{
-                buttonId: `${prefix}help`,
-                buttonText: {
-                    displayText: `${prefix}help`
-                },
                 type: 1,
             },];
             let bmffg = {
